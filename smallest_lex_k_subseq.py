25d070bf0ae6c78d75781090b9d98d5e99da4a8a
saswata <saswat.dutta@gmail.com>
2021-05-26 14:16:05 +0530

    adds smallest lexicographic k size subseq

---
+++ smallest_lex_k_subseq.py
@@ -0,0 +1,29 @@
+# return the lexicographically smallest subsequence of length k.
+
+def fix_stack(i, x, stack, k, n):
+    unused = n - 1 - i
+    while len(stack) > 0 and x < stack[-1] and (len(stack) + unused) >= k:
+        stack.pop()
+
+def find_smallest_subseq(nums, k):
+    if k <= 0:
+        return []
+    
+    n = len(nums)
+    stack = []
+
+    for i, x in enumerate(nums):
+        fix_stack(i, x, stack, k, n)
+        stack.append(x)
+    
+    return stack[:k]
+
+
+
+nums = [10, 1, 0]
+k = 2
+print(find_smallest_subseq(nums, k))
+
+nums = [1, 2, 0, 9, 2, 3]
+k = 3
+print(find_smallest_subseq(nums, k))
